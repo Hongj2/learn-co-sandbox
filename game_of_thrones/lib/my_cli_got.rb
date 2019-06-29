419d6ff0d73908ca4ed616650983bd5080b17ed7
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-06-29 22:27:12 +0000

    welcome page work

--- game_of_thrones/lib/my_cli_got.rb
+++ game_of_thrones/lib/my_cli_got.rb
@@ -1,75 +1,14 @@
+
 require 'nokogiri'
 require 'colorize'
 
 class GOT
-  
-  def run 
-    welcome
-    list_of_house
-    general_info
-    menu
+  def run
+    "running"
   end
-  
-  def welcome
-    puts ""
-    puts"       |>      |>                     |>      |>".bold
-    puts"       |_-___-_|                      |_-___-_|".bold
-    puts"        |vvvvv|                        |vvvvv|".bold
-    puts"       |__[[]]_|                      |_[[]]__|".bold
-    puts"      |   |**|  |_-_-_-_-_-_-_-_-_-_-_| |**|  |".bold
-    puts"     |    |  |   vvvvvvvvvvvvvvvvvvvvv  |  |   |".bold
-    puts"     |    |XX|          GAME OF         |XX|   |".bold
-    puts"     |    |()|          THRONES         |()|   |".bold
-    puts"     |     \/      vvvvvvvvvvvvvvvvv     \/    |".bold
-    puts"     |                  +++++                  |".bold
-    puts"     |                _/     \_                |".bold
-    puts"     |               _|   O   |_               |".bold
-    puts"     |               _|  ooo  |_               |".bold
-    puts"     |mmmmmmmmmmmmmmmm|       |mmmmmmmmmmmmmmmm|".bold
-    puts ""
-    puts "To see the list of the great houses of Westeros, type: list."
-    puts "To see the information of all the houses of Westeros, type: all"
-  end 
-  
-  def list_of_house
-    input=gets.strip
-    if input== "list"
-      list = Scraper.house.text.split("House")
-      list.each_with_index{|house,index| puts "[#{index+1}] #{house}"}
-    end
-    list
-end
-
-  def general_info
-    input=gets.strip
-    if input == "all"
-      all = Scraper.house.text.split("\n")
-      all.each_with_index{|house,index| puts "[#{index+1}] #{house}"}
-    elsif input == "exit"
-    system "clear" or sys "cls"
-    exit
-  else
-    puts "give me something to work..Try again".bold
-    
-    end
-  all
-end
-
-  def menu
-    input=gets.strip
-end
- 
-  def sigils
-    input=gets.strip
-end
-
-def motto
-  input=gets.strip
-end
 
 
+end 
 
 
-
-end #(f)
 
